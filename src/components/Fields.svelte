b39c6c9c3551a7e608021a7fe6b546ff0564c43a
Saud Kadiri <saudkadiri@gmail.com>
2022-06-14 22:18:25 +0530

    Welcome message

--- src/components/Fields.svelte
+++ src/components/Fields.svelte
@@ -1,7 +1,13 @@
 <script>
     import Field from "./Field.svelte";
+    import Navbar from "./Navbar.svelte";
     export let fields
 </script>
+
+<Navbar />
+
+<h1>Welcome to my website.😊</h1>
+<h1>So glad to have you!💛</h1>
 
 {#each fields as field }
     <Field {field}/>
