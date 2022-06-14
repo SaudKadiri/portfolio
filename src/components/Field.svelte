16ae767df3fde205b1ee608bfaf64a7c0db500e0
Saud Kadiri <saudkadiri@gmail.com>
2022-06-14 15:34:11 +0530

    About section

--- src/components/Field.svelte
+++ src/components/Field.svelte
@@ -1,8 +1,23 @@
 <script>
     import Card from "./Card.svelte";
     export let field
+    let src = "src/routes/saud-dnyandeep.jpg"
 </script>
 
 <Card>
-    {field}
+    <h3>{field.title}</h3>
+    {#if field.title === 'About'}
+        <img {src} class="img" alt="Saud's selfie at his favorite place." height="250dp"/>
+    {/if}
+    <br>
+    <p><em>{field.description}</em></p>
 </Card>
+
+<style>
+    .img {
+        border: 1px solid black;
+        border-radius: 3px;
+        margin-left: 25px;
+        margin-right: auto;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,31 @@
     import global from '../global.css'
     import Fields from "../components/Fields.svelte";
 
-    let fields = ['Programming Languages', 'Frameworks and Tools', 'App development', 'Courses']
+    // let fields = ['Programming Languages', 'Frameworks and Tools', 'App development', 'Courses']
+    let fields = [
+        {
+            title: 'About',
+            description: 'Hey, I\'m Saud. I\'m a Computer Engineer and I do System programming. I\'m also highly skilled in AI. I have practiced Data Structures and Algorithms throughly. I\'m interested in Computer Architectures(ARM), Extended Reality(XR), and low-level computer things. I prefer using Unix-based Operating Systems; currently using Apple macOS and have experienced Ubuntu(Debian) and Fedora in the past. Other than computer science I love Philosophy, Nature and "Good" people.',
+        }, 
+        {
+            title: 'Skills',
+            description: ''
+        }, 
+        {
+            title: 'Projects',
+            description: ''
+        }, 
+        {
+            title: 'Education',
+            description: ''
+        }, 
+        {
+            title: 'Profiles',
+            description: ''
+        }
+    ]
 </script>
 
-<main>
+<main class="container">
     <Fields {fields}/>
 </main>
